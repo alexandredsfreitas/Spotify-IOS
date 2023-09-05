39a67d4689bac7483780bf0c87a3f83325d5eb33
alexandredsfreitas <alexandredsfreitas@hotmail.com>
2023-09-05 17:33:01 -0300

    First commit with an initial structure and sufficient code to the first project run

---
+++ Spotify/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Alexandre de Souza Freitas on 05/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
